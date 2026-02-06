46d7c7063746de9950f96266c0dc6dac448a384a
ixsahithmasna <sahith.masna@innovatechs.com>
2026-02-06 11:29:14 +0530

    task3

--- 05-02-2026/SQLQuery3.sql
+++ 05-02-2026/SQLQuery3.sql
@@ -9,18 +9,24 @@ left join Orders as o on
 c.customer_id = o.customer_id
 where o.order_id is null
 
+select * from Employees 
+select * from Department
+
+select * from Department as d
+left join Employees as e  on 
+d.dep_id = e.dep_id
 
 
-select * from Customers as c
-join Orders as o 
-on c.customer_id = o.customer_id
-join products as p on 
-p.order_id = o.order_id
 
 
-select * from Customers as c 
-left join Orders as o on 
-c.customer_id = o.customer_id
-where c.name like 'sa%'
+select p.* from products as p
+left join order_items as oi
+on p.product_id = oi.product_id 
+where oi.order_id is null
+
+
+
+
+
 
 
