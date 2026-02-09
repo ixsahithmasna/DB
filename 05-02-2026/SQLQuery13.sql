b829084a71f5275b34e63b84f3d2133f136632df
ixsahithmasna <sahith.masna@innovatechs.com>
2026-02-09 09:46:20 +0530

    Task13

---
+++ 05-02-2026/SQLQuery13.sql
@@ -0,0 +1,26 @@
+--JOIN + LIKE Tasks
+--Customers starting with ‘s’ with orders
+--Employees name LIKE pattern + department
+--Products LIKE filter with order data
+
+
+select * from Customers as c 
+join Orders as o on 
+c.customer_id = o.customer_id 
+where c.name like 's%'
+
+
+select * from Employees as e 
+join Department as d on 
+e.dep_id = d.dep_id where e.emp_name like 'A%' or d.dep_name like 'E%'
+
+select * from Products as p 
+join Order_Items as ot on 
+p.product_id = ot.product_id where p.price>1000 order by p.product_id
+
+
+
+
+
+
+
